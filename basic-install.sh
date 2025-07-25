6ddedc9bffaa5db5b23a282364a503553634ac14
LazyNoob <100078891+pritipriya-dasbehera@users.noreply.github.com>
2025-07-25 19:08:55 +0530

    Update miktex install in basic-install.sh

--- basic-install.sh
+++ basic-install.sh
@@ -6,8 +6,8 @@ echo "installing brave now"
 curl -fsS https://dl.brave.com/install.sh | sh
 
 echo "installing miktek now"
-curl -fsSL https://miktex.org/download/key | sudo tee /usr/share/keyrings/miktex-keyring.asc > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/miktex-keyring.asc] https://miktex.org/download/debian bookworm universe" | sudo tee /etc/apt/sources.list.d/miktex.list
+curl -fsSL https://miktex.org/download/key | sudo gpg --dearmor -o /usr/share/keyrings/miktex.gpg
+echo "deb [signed-by=/usr/share/keyrings/miktex.gpg] https://miktex.org/download/debian bookworm universe" | sudo tee /etc/apt/sources.list.d/miktex.list
 sudo apt-get update
 sudo apt-get install miktex
 miktexsetup finish
